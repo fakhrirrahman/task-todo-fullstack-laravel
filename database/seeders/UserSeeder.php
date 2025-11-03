e42d2e9f99bf5925e5c617cdc951e85b87362582
fakhrirrahman <fakhrirrahman7@gmail.com>
2025-11-04 00:10:55 +0700

    Refactor code structure for improved readability and maintainability

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = ['pelaksana', 'leader', 'manager'];
+
+        foreach ($roles as $roleName) {
+            Role::firstOrCreate(['name' => $roleName]);
+        }
+    }
+}
